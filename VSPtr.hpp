a4fcc93f2877e122b399fc3e9b1f01ea787fde7a
mat <matrj08@gmail.com>
2020-06-03 02:27:03 -0600

    Se agrega destructor de vspointer
    Garbage collector en blanco :c

--- VSPtr.hpp
+++ VSPtr.hpp
@@ -1,11 +1,18 @@
 
+#include <iostream>
+#include <random>
+using namespace std;
+
 template <typename T>
 class VSPTR{
 private:
     T *dato;
+    int id;
     VSPTR(){
         dato = new T();
+        id = -1;
     };
+    
     
 
 
@@ -13,20 +20,28 @@ public:
     static VSPTR<T> New(){
         return VSPTR<T>();
     };
+    
+    ~VSPTR(){
+        if(this->id != -1){
+            delete[] dato;
+            cout<<"se elimino";
+        }
+    };
+
     VSPTR<T> operator *(){
         return *this;
-    }
-    // T& operator=()
+    };
+
     void  operator =(T dato){
         *this->dato = dato;
-    }
+    };
+
 
     T operator&(){
         return *(dato);
-    }
-    /*void operator &(VSPTR){
-        return this.dato;
-    }*/
+    };
+
+    
 
 };
 
